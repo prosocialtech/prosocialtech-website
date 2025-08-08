93e0c84df9bb2e7c4773bb00b51c766f1d842708
Briauna McGowan <briauna.mcgowan@gmail.com>
2025-08-07 20:32:20 -0400

    styling: add Karma Suture font and section heading styles with theme tokens

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import copy from '../../content/copy.json'
 function App() {
   return (
     <div className="bg-blue-900">
-      <h1 className="text-body">{copy.hero.title}</h1>
+      <h1 className="section-heading-primary">{copy.hero.title}</h1>
     </div>
   )
 }
