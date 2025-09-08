085cefb21d0f0d6fb026c5285997ee7d6d09127e
Gillian Black <blackg33@gmail.com>
2025-09-08 09:15:37 -0400

    Chore: update vite base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(() => ({
   plugins: [react(), tailwindcss()],
-  // Update to the below base with ternary once custom domain added
-  // Add {mode} back in as function arg
-  // base: mode === 'production' ? '/' : '/prosocialtech-website/',
-  base: '/prosocialtech-website/',
+  base: '/',
 }))
